d64823a6aa5a578ffdcb07783932fd2842fee7fe
Keita Akutsu <kakts.git@gmail.com>
2018-04-01 23:38:26 +0900

    Add a Stateful RandomWords Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,27 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(), // this highlighted text
         ),
       ),
     );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+// This maintains the state for RandomWords widget.
+// This class will save the generated word pairs, which grow infinitely as the use scrolls
+// and also favorite word pairs, as the user adds or removes them from the list by toggling the heart icon.
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
